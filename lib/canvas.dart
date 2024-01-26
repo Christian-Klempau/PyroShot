4e8c8776359e629d135355bf57677fdd2409b51b
Christian Klempau <christian.klempau@uc.cl>
2024-01-25 23:03:01 -0300

    feat: paint mode selection

--- lib/canvas.dart
+++ lib/canvas.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:ui' as ui;
 
 import 'package:screen_capturer/screen_capturer.dart';
@@ -15,6 +17,11 @@ enum MouseKind {
   up,
 }
 
+enum PaintKind {
+  line,
+  rect,
+}
+
 class MouseEvent {
   final Offset offset;
   final MouseKind kind;
@@ -25,35 +32,67 @@ class MouseEvent {
 class MyCanvas extends StatefulWidget {
   final CapturedData imageData;
   final Color currentColor;
+  final PaintKind paintMode;
 
   const MyCanvas(
-      {super.key, required this.imageData, required this.currentColor});
+      {super.key,
+      required this.imageData,
+      required this.currentColor,
+      required this.paintMode});
 
   @override
-  _MyCanvasState createState() =>
-      _MyCanvasState(imageData: imageData, currentColor: currentColor);
+  _MyCanvasState createState() => _MyCanvasState(
+      imageData: imageData, currentColor: currentColor, paintMode: paintMode);
 }
 
 class _MyCanvasState extends State<MyCanvas> {
   CapturedData imageData;
   Color currentColor;
+  PaintKind paintMode;
+  late CustomPainter currentPainter;
+
   final _notifier = ValueNotifier<MouseEvent>(MouseEvent(
     offset: Offset.zero,
     kind: MouseKind.dummy,
   ));
-  late RectPainter currentPainter;
 
-  _MyCanvasState({required this.imageData, required this.currentColor});
+  _MyCanvasState(
+      {required this.imageData,
+      required this.currentColor,
+      required this.paintMode});
+  GlobalKey globalKey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
-    currentPainter = RectPainter(notifier: _notifier, color: currentColor);
+    switch (paintMode) {
+      case PaintKind.line:
+        currentPainter = PointPainter(notifier: _notifier, color: currentColor);
+        break;
+      case PaintKind.rect:
+        currentPainter =
+            RectPainter(notifier: _notifier, color: currentColor);
+        break;
+    }
+  }
+
+  Future<Uint8List> captureImage() async {
+    RenderRepaintBoundary boundary =
+        globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    if (byteData == null) {
+      return Uint8List(0);
+    }
+    Uint8List pngBytes = byteData.buffer.asUint8List();
+    // save to disk
+    File imgFile = File('/home/chris/Pictures/test.png');
+    imgFile.writeAsBytes(pngBytes);
+    return pngBytes;
   }
 
   @override
   Widget build(BuildContext context) {
-    print("BUILDING");
     return Listener(
       onPointerMove: (event) => {
         _notifier.value = MouseEvent(
@@ -73,20 +112,18 @@ class _MyCanvasState extends State<MyCanvas> {
           kind: MouseKind.up,
         ),
       },
-      // child: CustomPaint(
-      //   painter: PointPainter(notifier: _notifier),
-      //   size: Size(1980, 1920),
-      //   child: imageData.imagePath != null
-      //       ? Image.file(File(imageData.imagePath!))
-      //       : Container(),
       child: imageData.imagePath != null
           ? Stack(
+              key: UniqueKey(),
               children: [
                 Image.file(File(imageData.imagePath!)),
                 CustomPaint(
                   key: UniqueKey(),
                   painter: currentPainter,
                 ),
+                Text(paintMode.toString(),
+                    style:
+                        TextStyle(background: Paint()..color = Colors.white)),
               ],
             )
           : Container(),
